32244bbb1fb59399e9a2d00a783246c4a1a976c2
Arthur <arthur91021@gmail.com>
2024-01-08 21:12:53 -0800

    9. Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,28 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is fun!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+let first = "jonas";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
