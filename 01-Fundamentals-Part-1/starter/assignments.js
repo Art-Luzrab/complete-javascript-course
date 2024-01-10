337c570dceecc273868a9b04ef6087cff09b9f4c
Arthur <arthur91021@gmail.com>
2024-01-09 21:09:02 -0800

    lecture 12

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,29 @@
+// JAVASCRIPT FUNDAMENTALS - PART 1
+
+//Lecture: Values and Variables
+
+/*
 let country = "Ukraine";
 let continent = "Europe";
 let population = 43700000;
 
 console.log(country, continent, population);
+console.log("--------------------------------------------------");
+
+//Lecture: Data Types
+
+let isIsland = "Ukraine";
+
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+console.log("--------------------------------------------------");
+*/
+
+//Lecture: let, const and var
+
+const language = "Ukrainian";
+console.log(language);
