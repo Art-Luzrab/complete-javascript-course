f9951bafb8d16fd388a3f7e52eb48ce332124615
Arthur <arthur91021@gmail.com>
2024-01-10 17:02:59 -0800

    lecture 22

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -60,4 +60,15 @@ console.log("19" - "13" + "17");
 console.log("19" - "13" + 17);
 console.log("123" < 57);
 console.log(5 + 6 + "4" + 9 - 4 - 2);
+
+
+const numNeighbors = Number(
+  prompt("How many neighbor countries does your country have?")
+);
+
+if (numNeighbors === 1) {
+  console.log("Only one neighbor!");
+} else if (numNeighbors > 1) {
+  console.log(" YOOOO DUDE IT HAS SO MANY NEIGHBORS!!!");
+}
 */
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -197,4 +197,26 @@ if (height) {
 } else {
   console.log("Height is UNDEFINED!");
 }
+
+const age = "18";
+if (age === 18) console.log("You just became an adult! (strict)");
+
+if (age == 18) console.log("You just became an adult! (loose)");
+
+const favorite = Number(prompt("What's your favorite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  // 23 === 23
+  console.log("Cool! 23 is an amazing number!");
+} else if (favorite === 7) {
+  console.log("7 is also a cool number!");
+} else if (favorite === 9) {
+  console.log("9 is a pretty chill number aswell.");
+} else {
+  console.log("Number is not 23 or 7");
+}
+
+if (favorite !== 23) console.log("Why not 23?");
 */
