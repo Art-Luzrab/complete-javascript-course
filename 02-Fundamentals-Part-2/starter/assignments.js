b232f75d5d4d7e2928cd11b6b97fc248df1148e8
Arthur <arthur91021@gmail.com>
2024-01-17 15:16:05 -0800

    lecture 34 Function Declarations vs Function Expressions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,3 +1,4 @@
+/** 
 function describeCountry(country, population, capitalCity) {
   const countryDescription = `${country} has ${population} million people and its capital city is ${capitalCity}.`;
   return countryDescription;
@@ -15,3 +16,25 @@ console.log(USA);
 
 const China = describeCountry("China", 1400, "Beijing");
 console.log(China);
+
+
+//Lecture Function Declarations vs. Expressions
+
+const worldPopulation = 7900;
+
+const percentageOfWorld1 = function (population) {
+  return (population / worldPopulation) * 100;
+};
+
+const ukrainePercentage1 = percentageOfWorld1(4.75);
+const americaPercentage1 = percentageOfWorld1(331);
+console.log(ukrainePercentage1, americaPercentage1);
+
+function percentageOfWorld2(population) {
+  return (population / worldPopulation) * 100;
+}
+
+const ukrainePercentage2 = percentageOfWorld2(4.75);
+const americaPercentage2 = percentageOfWorld2(331);
+console.log(ukrainePercentage2, americaPercentage2);
+*/
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -33,4 +33,22 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+
+// Lecture 34 Function Declarations vs. Expressions
+
+//Function Declaration
+
+function calcAge1(birthyear) {
+  return 2037 - birthyear;
+}
+const age1 = calcAge1(1991);
+
+//Function Expression
+const calcAge2 = function (birthyear) {
+  return 2037 - birthyear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
 */
