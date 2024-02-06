99c562f060982c2815ca92f1efee09399432eeed
Arthur <arthur91021@gmail.com>
2024-02-06 14:33:56 -0800

    course finished

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -67,4 +67,82 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew(temperatures, temperatures2);
 console.log(amplitudeNew);
+
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // C) FIX
+    // value: Number(prompt("Degrees Celcius:")),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) Identify
+console.log(measureKelvin());
+
+// Using a debugger
+const bug = function (t1, t2) {
+  const array1 = ["a", "b", "c"];
+  const array2 = ["d", "e", "f"];
+  const array3 = array1.concat(array2);
+
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    debugger;
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(min, max);
+  return max - min;
+};
+
+const amplitudeBug = bug([3, 5, 1], [9, 4, 5]);
+
+// A) IDENTIFTY
+console.log(amplitudeBug);
 **/
+
+//Challenge
+
+//Given an array of forecasted maximun temperatures, the thermometer displays a string with these temperatures.
+
+//Example: [17, 21, 23] will print: "... 17C in 1 days ... 21C in 2 days... 23C in 3 days ... "
+
+//Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+// Test Data 1: [17, 21, 23]
+// Test Data 2: [12, 5, -5, 0, 4]
+
+const testData1 = [17, 21, 23];
+const testData2 = [12, 5, -5, 0, 4];
+// console.log(testData1.indexOf(17) + 1);
+
+const printForecast = function (arr) {
+  let str = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}°C is in ${i + 1} days ... `;
+  }
+  console.log("... " + str);
+};
+
+printForecast(testData1);
+// printForecast(testData2);
