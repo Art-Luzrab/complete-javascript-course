b1a617006776e6da8369c11ac788f9df0f066938
Arthur <arthur91021@gmail.com>
2024-03-20 18:05:10 -0700

    Lecture 108. Short Curcuiting (&& and ||)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,38 @@ const restaurant = {
   },
 };
 /*
+///////////////////////////////////////////
+// Short Circuiting {&& and ||}
+
+console.log('---- OR ----');
+// Use ANY data type, return Any data type, short-curcuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'hello' || 23 || null);
+
+restaurant.numGuests = 0;
+
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+// Practical Example
+if (restaurant.orderPizza) {
+  restaurant.orderPasta('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 ////////////////////////////////////////////////////
 // The Rest Operator
 // 1.) Destructuring
