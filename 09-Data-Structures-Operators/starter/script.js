807d45ad6c88679af49d185c386a562dd28f797d
Arthur <arthur91021@gmail.com>
2024-03-22 15:00:02 -0700

    Lecture 109. Nullish Operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,18 @@ const restaurant = {
   },
 };
 /*
+//////////////////////////////////////////////
+// Null Operator
+restaurant.numGuests = 0;
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+
 ///////////////////////////////////////////
 // Short Circuiting {&& and ||}
 
