7411e1b20c7e5d2b09b56d8ff70946ff1b32b09c
Arthur <arthur91021@gmail.com>
2024-03-25 20:14:01 -0700

    lecture 112. For Of Loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -51,7 +51,17 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+///////////////////////////////////////////////////
+// For Of Loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+
+console.log([...menu.entries()]);
 /*
 ////////////////////////////////////////////////
 // Logical Assignment Operators
