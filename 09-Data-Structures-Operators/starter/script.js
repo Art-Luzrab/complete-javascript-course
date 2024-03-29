b8312f0205b7c97bfd5a69607c0b3c6b4b1ac0cc
Arthur <arthur91021@gmail.com>
2024-03-29 10:55:34 -0700

    Lecture 117. Sets

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,69 @@ const restaurant = {
   },
 };
 
+// Property NAMES
+const properties = Object.keys(openingHours);
+// console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+// console.log(openStr);
+
+// Property Values
+const values = Object.values(openingHours);
+// console.log(values);
+
+// Entire Object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}.`);
+// }
+
+/////////////////////////////////////////////////
+// Sets
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('ArthurBarzul').size);
+/*
+
+//////////////////////////////////////////////////////
+// Optional Chaining
 if (restaurant.openingHours && restaurant.openingHours.mon)
   console.log(restaurant.openingHours.mon.open);
 
@@ -81,7 +144,6 @@ console.log(users[0]?.name ?? 'User array Empty');
 
 if (users.length > 0) console.log(users[0].name);
 else console.log('user array empty');
-/*
 ///////////////////////////////////////////////////
 // For Of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
