33d046cdd120c72d880dfd27731ffbbc8ee827a9
Arthur <arthur91021@gmail.com>
2024-04-06 15:57:36 -0700

    Lecture 139. More Closure Examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -334,7 +334,7 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 // Bonus
 // BONUS TEST DATA 1: [5, 2, 3]
 // BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
-*/
+
 
 // Lecture 137. Immediately Ivoked Function Expressions (IIFE)
 
@@ -360,3 +360,67 @@ runOnce();
 
 // console.log(isPrivate);
 console.log(notPrivate);
+*/
+
+// Lecture 138. Closures
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+// Closure: makes a function remember all the variables that existed at the functions birthplace
+
+console.dir(booker);
+
+// Lecture 139. More Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers.`);
+  }, wait * 1000);
+
+  console.log(`Will Start boarding in ${wait} seconds.`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
