bae02c4287aea4385c81b144e876ebdb82ba529d
Arthur <arthur91021@gmail.com>
2024-04-19 16:28:26 -0700

    Lecture 161. The findIndex method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -183,6 +183,29 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+// Lecture 161. The findIndex() method
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+    //.indexOf(23)
+
+    //Delete ACcount
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -546,3 +569,5 @@ for (const account of accounts) {
   }
 }
 */
+
+//
