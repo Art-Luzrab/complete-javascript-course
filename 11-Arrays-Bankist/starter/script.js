ec01e2ec94188385f8808d54e96333e9b0cda385
Arthur <arthur91021@gmail.com>
2024-04-20 12:44:46 -0700

    Lecture 165. More Ways to Fill and Create Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -644,7 +644,7 @@ const overallBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overallBalance);
-*/
+
 // Lecture 164. Sorting Arrays
 
 // sort() mutates array
@@ -680,3 +680,38 @@ console.log(movements);
 // faster way
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+
+// Lecture 165. More Ways of Creating and Filling Arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+
+// x.fill(1, 3); // start filling with 1's at index 3
+x.fill(1, 3, 5); // start filling at index 3 but ends at index 5 ()
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6); // fill with 23's at index 4, ends at index 6
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y); // [1, 1, 1, 1, 1, 1, 1]
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z); // [1, 2, 3, 4, 5, 6, 7]
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
