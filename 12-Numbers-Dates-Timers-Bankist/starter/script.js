d5494a6c03a51aa462da22b7b1e487056e285cfd
Arthur <arthur91021@gmail.com>
2024-04-25 13:32:55 -0700

    Lecture 173. The Remainder Operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -343,4 +343,27 @@ console.log((2.345).toFixed(2)); // 2.35 (returns string)
 console.log(+(2.345).toFixed(2)); // 2.35 (number)
  */
 
-// 173. The Remainder Operatior
+// 173. The Remainder Operatior %
+
+console.log(5 % 2); // 1
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3); // 2
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(7 % 2); // 1
+console.log(7 / 2); // 3.5
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8)); // true
+console.log(isEven(23)); // false
+console.log(isEven(514)); // true
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
