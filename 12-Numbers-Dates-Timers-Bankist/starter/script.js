fd4a64beee7363dbde7f9fb72a9b50717b98299c
Arthur <arthur91021@gmail.com>
2024-04-25 13:20:56 -0700

    Lecture 172. Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,6 +252,7 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+/**
 // Lecture 171: Converting and Checking Numbers
 
 console.log(23 === 23.0);
@@ -290,3 +291,56 @@ console.log(Number.isFinite(23 / 0)); // false
 
 console.log(Number.isInteger(23)); // true
 console.log(Number.isInteger(23 / 0)); // false
+
+
+// Lecture 172. Math and Rounding
+
+// square roots and cubes
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+console.log(8 ** (1 / 3)); // 2
+
+// Math.max()
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+// Math.min()
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+// Math.PI()
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.159...
+
+console.log(Math.trunc(Math.random() * 6) + 1); // dice roll
+
+// function that randomizes numbers based on min and max
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1  ->     0...(max-min)  ->  min...(max - min + min)
+// console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+// round up
+console.log(Math.ceil(23.9)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+// round down
+console.log(Math.floor(23.9)); // 23
+console.log(Math.floor(23.9)); // 23
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Rounding Decimals
+console.log((2.7).toFixed(0)); // 3 ( returns string)
+console.log((2.7).toFixed(3)); // 2.700 (returns string)
+console.log((2.345).toFixed(2)); // 2.35 (returns string)
+console.log(+(2.345).toFixed(2)); // 2.35 (number)
+ */
+
+// 173. The Remainder Operatior
