e785a6d51e5cf6a0ba4155139ce4e4a62978581f
Arthur <arthur91021@gmail.com>
2024-05-17 17:58:09 -0700

    Lecture 254. Chaining Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,190 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/** 
+// Lecture 249. Our First AJAX Call: XMLHttpRequest
+
+// old school
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>
+        `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
+
+// Lecture 251. Welcome to Callback Hell (MWAHAHAHAHAHAHAHAH)
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+              }</p>
+            </div>
+          </article>
+          `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+    // AJAX call country 2
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('usa');
+
+// callback hell 😈
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+    }, 1000);
+    setTimeout(() => {
+      console.log('4 second passed');
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+
+// Lecture 252. Promises and the Fetch API
+
+// old way
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+// Fetch API (modern way)
+const request = fetch('https://restcountries.com/v2/name/portugal');
+console.log(request);
+*/
+
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>
+            `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+// Lecture 253. Consuming Promises
+// Lecture 254. Chaining Promises
+
+// cleaner, arrow functions
+
+const getCountryData = function (country) {
+  // Country 1
+  const request = fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+getCountryData('portugal');
+
+// messier (not arrow functions)
+
+// const getCountryData = function (country) {
+//   const request = fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+// getCountryData('portugal');
